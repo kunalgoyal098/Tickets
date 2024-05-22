6c8e31ab46e0e99162075c7261b46ea9c90c01a8
Kunal Goyal <148644214+kunalgoyal098@users.noreply.github.com>
2024-05-23 00:41:26 +0530

    Update ticket.sol

--- ticket.sol
+++ ticket.sol
@@ -1,57 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
-contract EventTicket{
-    address manager;
-    address User;
-    uint NumberOfTicket;
-    uint ticketPrice;
-    uint ticketRemaining;
-    uint startAt;
-    uint endAt;
-    uint quantity;
 
-   modifier theManager {
-    require(manager == msg.sender,"You are not the manager");
-    _;
-   }
-   constructor() {
+contract EventTicket {
+    address public manager;
+    uint public numberOfTickets;
+    uint public ticketPrice;
+    uint public startAt;
+    uint public endAt;
+    bool public saleActive;
+    uint public maxPurchaseLimit;
+    
+    mapping(address => uint) public ticketBalances;
+
+    modifier onlyManager {
+        require(manager == msg.sender, "You are not the manager");
+        _;
+    }
+
+    modifier saleIsActive {
+        require(saleActive, "Ticket sale is not active");
+        require(block.timestamp >= startAt && block.timestamp <= endAt, "Sale period is not active");
+        _;
+    }
+
+    constructor() {
         manager = msg.sender; // Set the deployer as the manager
     }
 
+    function set(uint _numberOfTickets, uint _ticketPrice, uint _startAt, uint _endAt, uint _maxPurchaseLimit) public onlyManager {
+        numberOfTickets = _numberOfTickets;
+        ticketPrice = _ticketPrice;
+        startAt = _startAt;
+        endAt = _endAt;
+        maxPurchaseLimit = _maxPurchaseLimit;
+        saleActive = true;
+    }
 
-   function set(uint _numberOfTicket, uint _ticketPrice ,uint _startAt, uint _endAt ) public theManager {
-    NumberOfTicket = _numberOfTicket;
-    ticketPrice = _ticketPrice;
-    
-    startAt = _startAt;
-    endAt = _endAt;
+    function buyTicket(uint _quantity) public payable saleIsActive {
+        require(_quantity <= numberOfTickets, "Not enough tickets available");
+        require(_quantity + ticketBalances[msg.sender] <= maxPurchaseLimit, "Exceeds maximum purchase limit");
+        require(msg.value == _quantity * ticketPrice, "Incorrect amount of ether sent");
 
+        ticketBalances[msg.sender] += _quantity;
+        numberOfTickets -= _quantity;
+    }
 
-   }
-   function buyticket (uint _quantity , uint date) public  payable {
-    quantity=_quantity;
-    date = block.timestamp;
-    require(NumberOfTicket !=0,"Not enough ticket");
-    require(date>=startAt && date <= endAt,"Sale ends");
-    require(msg.value == _quantity * ticketPrice,"less ether to buy ticket");
-    require(_quantity <= NumberOfTicket,"ticket are not that much");
-   NumberOfTicket -=quantity;
+    function getTicketBalance(address user) public view returns (uint) {
+        return ticketBalances[user];
+    }
 
-   }
-   function ticketReamain() public view returns(uint){
-    return NumberOfTicket;
-     
-   }
-   function toatalbalance() public view theManager returns(uint){
-    return address(this).balance;
-   }
-   
+    function setSaleActive(bool _saleActive) public onlyManager {
+        saleActive = _saleActive;
+    }
 
-   function withdraw(uint amount) external theManager{
-    require(amount<=address(this).balance,"insufficient Balance");
-    payable(manager).transfer(amount);
+    function remainingTickets() public view returns (uint) {
+        return numberOfTickets;
+    }
 
-   }
+    function totalBalance() public view onlyManager returns (uint) {
+        return address(this).balance;
+    }
 
-   }
+    function withdraw(uint amount) external onlyManager {
+        require(amount <= address(this).balance, "Insufficient balance");
+        payable(manager).transfer(amount);
+    }
+
+    function refundTickets(uint _quantity) public {
+        require(ticketBalances[msg.sender] >= _quantity, "Not enough tickets to refund");
+
+        uint refundAmount = _quantity * ticketPrice;
+        ticketBalances[msg.sender] -= _quantity;
+        numberOfTickets += _quantity;
+
+        payable(msg.sender).transfer(refundAmount);
+    }
+
+    function transferTickets(address _to, uint _quantity) public {
+        require(ticketBalances[msg.sender] >= _quantity, "Not enough tickets to transfer");
+
+        ticketBalances[msg.sender] -= _quantity;
+        ticketBalances[_to] += _quantity;
+    }
+
+    function getEventDetails() public view returns (uint, uint, uint, uint, bool, uint) {
+        return (numberOfTickets, ticketPrice, startAt, endAt, saleActive, maxPurchaseLimit);
+    }
+}
+
 
